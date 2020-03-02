cc0cda5adc93b62f852cd2f636b1ced5d0b7a17a
Chris Gray <christophermjgray@gmail.com>
2020-03-01 23:25:33 -0500

    Add files via upload

--- install_servers.sh
+++ install_servers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 clear
-# Version: 0.0.8
+# Version: 0.0.9
 # Updated: 2/25/2020
 #---------------------------
 # Software Installed
@@ -10,6 +10,7 @@ clear
 
 echo "Install Basic software (not servers)... "
 sudo apt-get install -y git software-properties-common build-essential 
+
 
 #-- install PHP 5.5 ---
 LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/PHP
@@ -26,23 +27,37 @@ sudo apt-get update
 sudo apt-get install -y mysql-server-5.6 mysql-client-5.6
 
 
-#-- install nginx 1.5.11
- # https://www.linuxbabe.com/nginx/nginx-latest-version-ubuntu-16-04-16-10
+#--- Requirements for Nginx ---
+# PCRE version 4.4 - 8.40
+wget https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz && tar xzvf pcre-8.40.tar.gz
+
+# zlib version 1.1.3 - 1.2.11
+wget http://www.zlib.net/zlib-1.2.11.tar.gz && tar xzvf zlib-1.2.11.tar.gz
+
+# OpenSSL version 1.0.2 - 1.1.0
+wget https://www.openssl.org/source/openssl-1.1.0f.tar.gz && tar xzvf openssl-1.1.0f.tar.gz
+
+
+#--- install Nginx 1.5.11 ---
+wget http://nginx.org/keys/nginx_signing.key
+sudo apt-key add nginx_signing.key
+
+# https://www.linuxbabe.com/nginx/nginx-latest-version-ubuntu-16-04-16-10
+# https://www.vultr.com/docs/how-to-compile-nginx-from-source-on-ubuntu-16-04
 
 # If you have installed Nginx from Ubuntu software repository, you need to remove it before executing the above two commands.
 #sudo apt remove nginx nginx-common nginx-full nginx-core
 
 wget https://nginx.org/download/nginx-1.5.11.tar.gz && tar zxvf nginx-1.5.11.tar.gz
 
+#rm -rf *.tar.gz
+
 # sudo nano /etc/apt/sources.list
 # deb http://nginx.org/packages/mainline/ubuntu/ xenial nginx
 # deb-src http://nginx.org/packages/mainline/ubuntu/ xenial nginx
 
-wget http://nginx.org/keys/nginx_signing.key
-sudo apt-key add nginx_signing.key
-
-#sudo apt update
-#sudo apt install nginx
+cd ~/nginx-1.5.11
+sleep 1
 
 ./configure --prefix=/usr/share/nginx \
             --sbin-path=/usr/sbin/nginx \
@@ -98,20 +113,32 @@ sudo apt-key add nginx_signing.key
 make 
 sudo make install
 
+cd ~
+rm -r nginx-1.5.11/ openssl-1.1.0f/ pcre-8.40/ zlib-1.2.11/
+
+sudo nginx -v && sudo nginx -V
+sudo nginx -t
+
+echo "Installing Nginx Service....  "
+mkdir -p /var/lib/nginx && sudo nginx -t
+#sudo vim /etc/systemd/system/nginx.service
+wget https://raw.githubusercontent.com/c2theg/Vulnerable_Server/master/nginx.service
+sudo mv nginx.service /etc/systemd/system/nginx.service
+sudo systemctl start nginx.service && sudo systemctl enable nginx.service
+sudo systemctl is-enabled nginx.service
 
 echo "Starting nginx... "
-
 sudo systemctl start nginx
 
 echo "sleeping for 5 seconds.."
 sleep 5
 
+sudo systemctl status nginx.service
+ps aux | grep nginx
+curl -I 127.0.0.1
 
-nginx -v
+#nginx -v
 systemctl status nginx
-
-
-
 #*****************************************************************************************************************************************************************
 WebsitePath=/var/www/html
 
@@ -127,17 +154,17 @@ git clone --recursive https://github.com/ethicalhack3r/DVWA.git
 sleep 2
 wait
 #--- Hackazon ---
-cd $WebsitePath/
 echo "Downloading Hackazon...  http://cybersecology.com/hackazon-review/  
 
 "
-cd $WebsitePath/
+cd ..
 mkdir hackazon
 cd hackazon/
 git clone --recursive https://github.com/rapid7/hackazon.git
 
 #--- Default home page ---
-cd $WebsitePath/
+cd ..
+#cd $WebsitePath/
 echo "Downloading Nginx Config"
 wget -O "nginx_global_filetypes.conf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/nginx_global_filetypes.conf"
 wget -O "nginx_global_logging.conf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/nginx_global_logging.conf"
@@ -158,7 +185,6 @@ sudo mv "nginx_global_security.conf" "/etc/nginx/snippets/nginx_global_security.
 sudo mv "nginx_global_tls.conf" "/etc/nginx/snippets/nginx_global_tls.conf"
 sudo mv "nginx.conf" "/etc/nginx/nginx.conf"
 #-- sample page --
-
 
 sudo mv "index.html" "$WebsitePath/index.html"
 sudo mv "custom_404.html" "$WebsitePath/custom_404.html"

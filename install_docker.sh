14d7b09956c637cf4884c3750d41fbf8ad0e5475
Chris Gray <christophermjgray@gmail.com>
2020-03-03 23:33:00 -0500

    Add files via upload

--- install_docker.sh
+++ install_docker.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-clear
-# Version: 0.0.1
+# Version: 0.0.2
 # Updated: 3/3/2020
+# Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
 #---------------------------
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt-get update
-
 apt-cache policy docker-ce
-
 sudo apt-get install -y docker-ce
-

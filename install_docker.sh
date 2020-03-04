53f754cf84637831bd6d68610c6f6b043f9921d5
Chris Gray <christophermjgray@gmail.com>
2020-03-03 23:34:32 -0500

    Add files via upload

--- install_docker.sh
+++ install_docker.sh
@@ -9,3 +9,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
+
+
+sudo systemctl status docker
+

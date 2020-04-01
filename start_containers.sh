def0aed76d995476c298c6b5f6a6117369d664a3
Chris Gray <christophermjgray@gmail.com>
2020-03-31 23:35:25 -0400

    Update start_containers.sh

--- start_containers.sh
+++ start_containers.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-# Version: 0.0.1
-# Updated: 3/9/2020
+# Version: 0.0.3
+# Updated: 3/31/2020
 #---------------------------
 echo "Starting Juice Shop... \r\n "
 docker run --rm -p 3000:80 bkimminich/juice-shop &
 echo "\r\n \r\n"
 echo "Browse to site: http://<Local IP> \r\n \r\n"
 
+
 echo "Starting Hackazon... \r\n "
 sudo docker run --name hackazon1 -d -p 8081:3001 ianwijaya/hackazon &
 echo "\r\n \r\n"
 echo "Browse to site: http://<Local IP>:3001 \r\n \r\n"
+
 
 echo "Starting DVWA... \r\n "
 docker run --rm -it -p 80:3002 vulnerables/web-dvwa &
@@ -23,9 +25,13 @@ docker run -d --dns=127.0.0.1 \
   --volume=/srv/docker/bind:/data \
   --env='ROOT_PASSWORD=Password123$' \
   sameersbn/bind:latest &
+
+
 echo "\r\n \r\n"  
 echo "Browse to Webmin from: https://<Local IP>:10000"
 
 echo "\r\n \r\n"
 echo "Cockpit is always avaiable at: http://<LOCAL IP>:9090"
 echo "\r\n \r\n"
+
+docker ps

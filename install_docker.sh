cac4c8df5133150c31dc51ab55f2790a1eb2b030
Chris Gray <christophermjgray@gmail.com>
2020-04-10 14:15:13 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,15 +1,37 @@
 #!/bin/sh
-# Version: 0.0.2
-# Updated: 3/3/2020
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  1.5.6                             \r\n
+Last Updated:  4/10/2020
+
+"
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
 # Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
-#---------------------------
+sudo apt-get install -y apt-transport-https software-properties-common ca-certificates
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+
 sudo apt-get update
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
+sudo apt  install -y docker-compose
 
-
+wait
 sudo systemctl status docker
-

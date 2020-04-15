5a17c57af463d6aedeaf6f52aa081b33f66a28a4
Chris Gray <christophermjgray@gmail.com>
2020-04-15 14:04:12 -0400

    Update start_containers.sh

--- start_containers.sh
+++ start_containers.sh
@@ -12,41 +12,63 @@ docker run -d --rm -p 9000:9000 -v "/var/run/docker.sock:/var/run/docker.sock" p
 echo "Browse to Portainer from: https://<Local IP>:9000"
 
 
-echo "Starting Juice Shop (3000) ... \r\n "
-docker run --rm -p 3000:3000 bkimminich/juice-shop &
-echo "\r\n \r\n"
-echo "Browse to site: http://<Local IP> \r\n \r\n"
-sleep 10
+echo "Starting Juice Shop (80) ... \r\n "
+docker run --rm -p 80:3000 bkimminich/juice-shop &
+echo "
 
-echo "Starting Hackazon (3001)... \r\n"
-docker run --rm -it -p 8081:3001 ianwijaya/hackazon &
-echo "\r\n \r\n"
-echo "Browse to site: http://<Local IP>:3001 \r\n \r\n"
+
+"
+echo "Browse to site: http://<Local IP>  PLEASE WAIT up to 15seconds before the server is ready \r\n \r\n"
+wait
 sleep 5
 
-echo "Starting DVWA (3003)... \r\n"
-docker run --rm -it -p 80:3003 vulnerables/web-dvwa &
-echo "\r\n \r\n"
-echo "Browse to site: http://<Local IP>:3003 \r\n \r\n"
+
+echo "Starting DVWA (81)... \r\n"
+docker run --rm -it -p 81:80 vulnerables/web-dvwa &
+echo "
+
+
+"
+echo "Browse to site: http://<Local IP>:81 \r\n \r\n"
+wait
 sleep 5
 
-echo "Starting Wordpress (3004)... \r\n"
-docker run --rm -it -p 8080:3004 wordpress &
-echo "\r\n \r\n"
-echo "Browse to site: http://<Local IP>:3004 \r\n \r\n"
+
+echo "Starting Wordpress (82)... \r\n"
+docker run --rm -p 82:80 wordpress &
+echo "
+
+
+"
+echo "Browse to site: http://<Local IP>:82 \r\n \r\n"
 sleep 5
+
+echo "Starting Hackazon (8081)... \r\n"
+docker run --rm -p 8081:80 ianwijaya/hackazon &
+echo "
+
+
+"
+echo "Browse to site: http://<Local IP>:8081 \r\n \r\n"
+sleep 5
+
 
 echo "Starting Bind DNS  (10000)... \r\n"
-docker run --rm -it --dns=127.0.0.1 \
-  --publish=53:53/udp --publish 53:53/tcp --publish=10000:10000/tcp \
-  --volume=/srv/docker/bind:/data \
+docker run -d --restart=always \
+  --publish 853:53/tcp --publish 853:53/udp --publish 10000:10000/tcp \
+  --volume /srv/docker/bind:/data \
   --env='ROOT_PASSWORD=Password123$' \
-  sameersbn/bind:latest &
-echo "\r\n \r\n"
-echo "Browse to Webmin from: https://<Local IP>:10000"
+  sameersbn/bind:latest
+ 
+echo "
 
 
-echo "\r\n \r\n"
+"
+echo "Browse to Webmin from: https://<Local IP>:10000  Login: root / Password123$ "
+echo "
+
+
+"
 echo "Cockpit is always avaiable at: http://<LOCAL IP>:9090"
 echo "
 

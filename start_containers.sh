665066a27182835f5372c7dbf1b58fe96625908a
Chris Gray <christophermjgray@gmail.com>
2020-04-15 12:00:44 -0400

    Update start_containers.sh

--- start_containers.sh
+++ start_containers.sh
@@ -1,44 +1,44 @@
 #!/bin/sh
-# Version: 0.0.7
+# Version: 0.0.8
 # Updated: 4/15/2020
 #---------------------------
-echo "Starting Juice Shop... \r\n "
-docker run --rm -p 3000:81 bkimminich/juice-shop &
+echo "Starting Portainer (9000)... \r\n"
+docker run -d --rm -it -p 9000:9000 -v "/var/run/docker.sock:/var/run/docker.sock" portainer/portainer &
+echo "Browse to Portainer from: https://<Local IP>:9000"
+
+
+echo "Starting Juice Shop (80) ... \r\n "
+docker run -d --rm -it -p 3000:80 bkimminich/juice-shop &
 echo "\r\n \r\n"
 echo "Browse to site: http://<Local IP> \r\n \r\n"
 
 
-echo "Starting Hackazon... \r\n"
-sudo docker run -d -p 8081:3001 ianwijaya/hackazon &
+echo "Starting Hackazon (81)... \r\n"
+docker run -d --rm -it -p 8081:81 ianwijaya/hackazon &
 echo "\r\n \r\n"
-echo "Browse to site: http://<Local IP>:3001 \r\n \r\n"
+echo "Browse to site: http://<Local IP>:81 \r\n \r\n"
 
 
-echo "Starting DVWA... \r\n"
-docker run --rm -it -p 80:3002 vulnerables/web-dvwa &
-echo "\r\n \r\n"
-echo "Browse to site: http://<Local IP>:3002 \r\n \r\n"
-
-
-echo "Starting Wordpress... \r\n"
-docker run -p 8080:82 -d wordpress &
+echo "Starting DVWA (82)... \r\n"
+docker run -d --rm -it -p 80:82 vulnerables/web-dvwa &
 echo "\r\n \r\n"
 echo "Browse to site: http://<Local IP>:82 \r\n \r\n"
 
 
-echo "Starting Bind DNS... \r\n"
-docker run -d --dns=127.0.0.1 \
+echo "Starting Wordpress (83)... \r\n"
+docker run -d --rm -it -p 8080:83 wordpress &
+echo "\r\n \r\n"
+echo "Browse to site: http://<Local IP>:83 \r\n \r\n"
+
+
+echo "Starting Bind DNS  (10000)... \r\n"
+docker run -d --rm -it --dns=127.0.0.1 \
   --publish=53:53/udp --publish 53:53/tcp --publish=10000:10000/tcp \
   --volume=/srv/docker/bind:/data \
   --env='ROOT_PASSWORD=Password123$' \
   sameersbn/bind:latest &
-echo "\r\n \r\n"  
+echo "\r\n \r\n"
 echo "Browse to Webmin from: https://<Local IP>:10000"
-
-
-echo "Starting Portainer... \r\n"
-docker run -d -p 9000:9000 -v "/var/run/docker.sock:/var/run/docker.sock" portainer/portainer &
-echo "Browse to Portainer from: https://<Local IP>:9000"
 
 
 echo "\r\n \r\n"
